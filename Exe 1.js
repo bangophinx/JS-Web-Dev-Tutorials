a38f92ac1873e1f58a85c45c3d39bcc2a3e59bf1
Evans Chikarakara <bangophinx@gmail.com>
2018-01-30 21:58:36 +0000

    Initial Commit

---
+++ Exe 1.js
@@ -0,0 +1,14 @@
+
+
+var firstName = prompt("What is Your First Name");
+var lastName = prompt("What is Your Last Name");
+var age = prompt("What is Your Age");
+var daysAlive = age*365;
+alert("Your full name is "+firstName +" " +lastName);
+console.log("Your full name is "+firstName +" " +lastName);
+alert("Your are " + age + " years old. Roughly " + daysAlive + " days old.");
+console.log("Your are " + age +"years old OR days alive: "+ daysAlive);
+
+var name
+
+
