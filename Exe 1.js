b187aa64124d0686e6491d55da7e2a08d81e44a5
Evans Chikarakara <bangophinx@gmail.com>
2018-01-31 18:22:32 +0000

    Update 1

--- Exe 1.js
+++ Exe 1.js
@@ -1,14 +1,43 @@
 
 // Put comments
-var firstName = prompt("What is Your First Name");
-var lastName = prompt("What is Your Last Name");
+// var firstName = prompt("What is Your First Name");
+// var lastName = prompt("What is Your Last Name");
 var age = prompt("What is Your Age");
 var daysAlive = age*365;
-alert("Your full name is "+firstName +" " +lastName);
-console.log("Your full name is "+firstName +" " +lastName);
-alert("Your are " + age + " years old. Roughly " + daysAlive + " days old.");
-console.log("Your are " + age +"years old OR days alive: "+ daysAlive);
+var enterClub;
 
-var name
+// alert("Your full name is "+firstName +" " +lastName);
+// console.log("Your full name is "+firstName +" " +lastName);
+// alert("Your are " + age + " years old. Roughly " + daysAlive + " days old.");
+// console.log("Your are " + age +"years old OR days alive: "+ daysAlive);
+
+if (age<0){
+  console.log("Error Age");
+}
+
+if (age%2!==0){
+  console.log("Your Age is odd");
+}
+
+if (Number.isInteger(Math.sqrt(age))){
+  console.log("Your Age is a perfect square");
+}
+
+if (age==21){
+  console.log("Happy 21st Birthday");
+}
+
+if(age<18){
+  console.log("Your can't enter club");
+  }
+  else if (age<21){
+    console.log("Your can enter club but cant drink");
+}
+
+  else {
+    console.log("Your can enter club and drink");
+}
+
+
 
 
